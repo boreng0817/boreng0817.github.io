2d8e63c566b1b641dc7b68f9fb457365d56d7d87
김시우 <boreng0817@hanyang.ac.kr>
2021-02-25 23:31:16 +0900

    done

--- script.js
+++ script.js
@@ -16,35 +16,34 @@ function setButtonColour(button, r, g, b){
 }
 
 function startGame() {
-    
-    var answer = Math.round(Math.random() * (buttons.length - 1));
-    answerMessage.innerHTML = "";
-    
-  for (var i = 0 ; i < buttons.length; ++i) {
-    var r = makeColourValue();
-    var g = makeColourValue();
-    var b = makeColourValue();
-    setButtonColour(buttons[i], r, g, b);
-    
-    if (i === answer) {
-      R = r;
-      G = g;
-      B = b;
-      heading.innerHTML = `(${r},${g},${b})`;
-    }
-
-    buttons[i].addEventListener('click', function(){
-      if (this === buttons[answer]) {
-        answerMessage.innerHTML = "정답!";
-        setButtonColour(body, R, G, B);
-      } else {
-        answerMessage.innerHTML = "땡!";
-      }
-    });
+  
+  var answer = Math.round(Math.random() * (buttons.length - 1));
+  setButtonColour(body, 255, 255, 255);
+  answerMessage.innerHTML = "";
+  
+for (var i = 0 ; i < buttons.length; ++i) {
+  var r = makeColourValue();
+  var g = makeColourValue();
+  var b = makeColourValue();
+  setButtonColour(buttons[i], r, g, b);
+  
+  if (i === answer) {
+    R = r;
+    G = g;
+    B = b;
+    heading.innerHTML = `(${r},${g},${b})`;
   }
+
+  buttons[i].addEventListener('click', function(){
+    if (this === buttons[answer]) {
+      answerMessage.innerHTML = "정답!";
+      setButtonColour(body, R, G, B);
+    } else {
+      answerMessage.innerHTML = "땡!";
+    }
+  });
 }
-
-
+}
 
 startGame();
 
